2811d6309aaf60af4fb6c6e6a0c1014c5ae479bc
Sven Marc Gehring <svengehring@cybrox.eu>
2015-03-10 14:15:06 +0100

    Added basic design and page structure

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -7,3 +7,21 @@
  */
 
 var App = Ember.Application.create();
+
+
+/** 
+ * Application router
+ *
+ * We're only using top level routes, since this will be
+ * hosten on GitHub pages and since we cannot load in
+ * templates dynamically, nested routes would be a pain to load.
+ */
+App.Router.map(function() {
+
+    // this.route('hardware-overview');
+    this.route('software-preparation');
+    this.route('software-installation');
+    this.route('software-examples');
+    this.route('software-documentation');
+
+});
